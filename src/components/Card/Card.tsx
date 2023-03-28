9e716c69b4ca76c1fee25877401a0fd8f4f780a6
NatalliaE <natasha.ehlis@gmail.com>
2023-03-28 14:55:02 +0300

    fix: rewrite Card with the hooks

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,55 +6,48 @@ type PropsType = {
   item: IProduct;
 };
 
-class Card extends React.Component<PropsType> {
-  render() {
-    return (
-      <div className={style.card} key={this.props.item.id}>
-        <div className={style.card__img}>
-          <img
-            className={style.image}
-            src={this.props.item.thumbnail}
-            alt={this.props.item.title}
-          />
+const Card = (props: PropsType) => {
+  const item = props.item;
+
+  return (
+    <div className={style.card} key={item.id}>
+      <div className={style.card__img}>
+        <img className={style.image} src={item.thumbnail} alt={item.title} />
+      </div>
+      <div className={style.card__description}>
+        <h2 className={style.card__title}>{item.title}</h2>
+        <div>
+          <b>Brand: </b>
+          {item.brand}
         </div>
-        <div className={style.card__description}>
-          <h2 className={style.card__title}>{this.props.item.title}</h2>
-          <div>
-            <b>Brand: </b>
-            {this.props.item.brand}
-          </div>
-          <div>
-            <b>Category: </b>
-            {this.props.item.category}
-          </div>
-          <div>
-            <b>Description: </b>
-            {this.props.item.description}
-          </div>
-          <div>
-            <div className={style.price}>
-              <div>
-                Price: <s>{this.props.item.price}$</s>
-              </div>
-              <div>
-                With discount:{' '}
-                <span className={style.price__discount}>
-                  {Math.round(
-                    (this.props.item.price / 100) * (100 - this.props.item.discountPercentage)
-                  )}
-                  $
-                </span>
-              </div>
+        <div>
+          <b>Category: </b>
+          {item.category}
+        </div>
+        <div>
+          <b>Description: </b>
+          {item.description}
+        </div>
+        <div>
+          <div className={style.price}>
+            <div>
+              Price: <s>{item.price}$</s>
             </div>
-            <div className={style.info}>
-              <div>Rating: {this.props.item.stock}</div>
-              <div>Stock: {this.props.item.price}</div>
+            <div>
+              With discount:{' '}
+              <span className={style.price__discount}>
+                {Math.round((item.price / 100) * (100 - item.discountPercentage))}$
+              </span>
             </div>
+          </div>
+          <div className={style.info}>
+            <div>Rating: {item.stock}</div>
+            <div>Stock: {item.price}</div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Card;
