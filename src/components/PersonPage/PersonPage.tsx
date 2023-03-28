dd8a397b3ccbab91ab84d7270e2cc66937a415c4
NatalliaE <natasha.ehlis@gmail.com>
2023-03-28 15:12:48 +0300

    fix: rewrite Person Page with the hooks

--- src/components/PersonPage/PersonPage.tsx
+++ src/components/PersonPage/PersonPage.tsx
@@ -1,41 +1,32 @@
 import AddPerson from 'components/AddPerson/AddPerson';
 import Person from 'components/Person/Person';
 import { IPerson } from 'components/state/IPerson';
-import React from 'react';
+import React, { useState } from 'react';
 import style from './PersonPage.module.css';
 
-type FormPageType = {
-  items: IPerson[] | [];
+const PersonPage = () => {
+  const [items, setItems] = useState<IPerson[]>([]);
+
+  const addCard = (card: IPerson) => {
+    const newItems: IPerson[] = [...items, card];
+    setItems(newItems);
+  };
+
+  return (
+    <div className={style.formPage}>
+      <div>Person</div>
+      <AddPerson addCard={addCard} ind={items.length} />
+      {items.length === 0 ? (
+        <div>No Person</div>
+      ) : (
+        <div className={style.persons}>
+          {items.map((item) => {
+            return <Person key={item.id} item={item} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
-
-class PersonPage extends React.Component {
-  state: FormPageType = {
-    items: [],
-  };
-
-  addCard = (card: IPerson) => {
-    const newState = this.state;
-    newState.items = [...this.state.items, card];
-    this.setState(newState);
-  };
-
-  render() {
-    return (
-      <div className={style.formPage}>
-        <div>Person</div>
-        <AddPerson addCard={this.addCard} ind={this.state.items.length} />
-        {this.state.items.length === 0 ? (
-          <div>No Person</div>
-        ) : (
-          <div className={style.persons}>
-            {this.state.items.map((item) => {
-              return <Person key={item.id} item={item} />;
-            })}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
 
 export default PersonPage;
