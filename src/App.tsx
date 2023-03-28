d5ffb36e2a2cedd80f5abab09cfd17a330631b63
NatalliaE <natasha.ehlis@gmail.com>
2023-03-28 14:24:29 +0300

    fix: rewrite App with the hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,23 @@ import './App.css';
 import { Route, Routes } from 'react-router';
 import AboutUs from 'components/AboutUs/AboutUs';
 import ErrorPage from 'components/ErrorPage/ErrorPage';
-import Main from 'components/Main/Main';
 import { Navigate } from 'react-router-dom';
 import { HeaderWithRouter } from 'components/Header/Header';
 import PersonPage from 'components/PersonPage/PersonPage';
+import Main from 'components/Main/Main';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <HeaderWithRouter />
-        <Routes>
-          <Route path="" element={<Navigate to="/main" replace />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/person" element={<PersonPage />} />
-          <Route path="/Error" element={<ErrorPage />} />
-          <Route path="*" element={<Navigate to="/Error" replace />} />
-        </Routes>
-      </div>
-    );
-  }
-}
-
-export default App;
+export const App = () => {
+  return (
+    <div className="App">
+      <HeaderWithRouter />
+      <Routes>
+        <Route path="" element={<Navigate to="/main" replace />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/person" element={<PersonPage />} />
+        <Route path="/Error" element={<ErrorPage />} />
+        <Route path="*" element={<Navigate to="/Error" replace />} />
+      </Routes>
+    </div>
+  );
+};
