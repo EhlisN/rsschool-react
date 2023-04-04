fc15b5ef9fc9450b316df11b650e77c01b00146a
NatalliaE <natasha.ehlis@gmail.com>
2023-04-04 14:11:25 +0300

    fix: rewrite About Us & Error Page

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import style from './AboutUs.module.css';
 
-class AboutUs extends React.Component {
-  render() {
-    return <div className={style.aboutUs}>About us</div>;
-  }
-}
+const AboutUs = () => {
+  return <div className={style.aboutUs}>About us</div>;
+};
 
 export default AboutUs;
--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import style from './ErrorPage.module.css';
 
-class ErrorPage extends React.Component {
-  render() {
-    return (
-      <div className={style.errorPage}>
-        404 <br /> Page Not Found
-      </div>
-    );
-  }
-}
+const ErrorPage = () => {
+  return (
+    <div className={style.errorPage}>
+      404 <br /> Page Not Found
+    </div>
+  );
+};
 
 export default ErrorPage;
