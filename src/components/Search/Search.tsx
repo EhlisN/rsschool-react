91b3ad0a2ee9dbc09d1c2549555169663c0fbc98
NatalliaE <natasha.ehlis@gmail.com>
2023-04-14 18:48:21 +0300

    fix: search with redux

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { setSearchValue } from 'redux/slices/searchSlice';
+import { useAppDispatch, useAppSelector } from 'redux/store';
 import style from './Search.module.css';
 
-type PropsTypeSearch = {
-  setValue: React.Dispatch<React.SetStateAction<string>>;
-  value: string;
-};
+const Search = () => {
+  const searchData = useAppSelector((state) => state.searchSlice.stateSearch);
+  const dispatch = useAppDispatch();
+  const [searchInput, setSearchInput] = useState(searchData);
 
-const Search = (props: PropsTypeSearch) => {
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    props.setValue(e.target.value);
+    e.preventDefault();
+    setSearchInput(e.target.value);
   };
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(setSearchValue(searchInput));
+  };
+
   return (
-    <div className={style.search}>
-      <span className={style.search__title}> &#128269;</span>
-      <input
-        className={style.search__input}
-        type="text"
-        placeholder="Search"
-        value={props.value || ''}
-        onChange={handleInputChange}
-        role="value"
-      />
-    </div>
+    <form onSubmit={handleFormSubmit}>
+      <div className={style.search}>
+        <span className={style.search__title}> &#128269;</span>
+        <input
+          className={style.search__input}
+          type="text"
+          placeholder="Search..."
+          value={searchInput}
+          onChange={handleInputChange}
+          role="value"
+        />
+        <button className={style.search__btn} type="submit">
+          Find
+        </button>
+      </div>
+    </form>
   );
 };
 
