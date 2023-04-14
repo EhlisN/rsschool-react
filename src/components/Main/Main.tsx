059f843bfa9cc38eb63ae6eeceda56512b5b1232
NatalliaE <natasha.ehlis@gmail.com>
2023-04-14 18:47:35 +0300

    fix: main with redux

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,41 +1,16 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import Card from 'components/Card/Card';
 import style from './Main.module.css';
-import { IProduct } from 'components/state/IProducts';
-import { getProducts } from 'components/Api/Api';
 import Preloader from 'utils/Preloader/Preloader';
 import Product from 'components/Product/Product';
 import Search from 'components/Search/Search';
+import { useGetProductsQuery } from 'redux/api/api';
+import { useAppSelector } from 'redux/store';
 
 const Main = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [value, setValue] = useState(localStorage.getItem('value') || '');
+  const searchData = useAppSelector((state) => state.searchSlice);
+  const { data: items = [], isLoading } = useGetProductsQuery(searchData.stateSearch);
   const [modal, setModal] = useState<null | number>(null);
-  const [items, setItems] = useState<IProduct[]>([]);
-  const valueRef = useRef<string>();
-
-  const getItems = useCallback(async (search: string): Promise<void> => {
-    setIsLoading(true);
-    try {
-      const data = await getProducts(search);
-      setItems(data);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    getItems(value);
-    valueRef.current = value;
-  }, [value]);
-
-  useEffect(() => {
-    return () => {
-      localStorage.setItem('value', valueRef.current || '');
-    };
-  }, []);
 
   const closeModal = () => {
     setModal(null);
@@ -52,17 +27,17 @@ const Main = () => {
         <></>
       )}
       <h2 className={style.name}>Main Page</h2>
-      <Search setValue={setValue} value={value} />
+      <Search />
       {isLoading ? (
         <Preloader />
       ) : (
         <div className={style.cards}>
-          {items.length > 0 ? (
+          {items.length ? (
             items.map((item) => {
               return <Card key={item.id} item={item} setModal={setModal} />;
             })
           ) : (
-            <div>Do not find - {value}</div>
+            <div>Do not find - {searchData.stateSearch}</div>
           )}
         </div>
       )}
